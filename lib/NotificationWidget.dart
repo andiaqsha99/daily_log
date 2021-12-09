6736c5f285060381c119befb227e1f87ff718237
andiaqsha99 <andi77aqsha@gmail.com>
2021-12-09 10:06:29 +0700

    hide badge counter

--- lib/NotificationWidget.dart
+++ lib/NotificationWidget.dart
@@ -8,21 +8,20 @@ class NotificationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var notifProvider = Provider.of<NotifProvider>(context);
-    return Badge(
-      position: BadgePosition.topEnd(end: 1, top: 2),
-      showBadge: true,
-      badgeContent:
-          Consumer<NotifCounterProvider>(builder: (context, provider, child) {
-        return Text(provider.counter.toString());
-      }),
-      child: IconButton(
-          onPressed: () => {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return NotificationPage(listNotif: notifProvider.listNotif);
-                }))
-              },
-          icon: Icon(Icons.notifications)),
-    );
+    return Consumer<NotifCounterProvider>(builder: (context, provider, child) {
+      return Badge(
+        position: BadgePosition.topEnd(end: 1, top: 2),
+        showBadge: provider.counter == 0 ? false : true,
+        badgeContent: Text(provider.counter.toString()),
+        child: IconButton(
+            onPressed: () => {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return NotificationPage(listNotif: notifProvider.listNotif);
+                  }))
+                },
+            icon: Icon(Icons.notifications)),
+      );
+    });
   }
 }
